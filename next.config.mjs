c9a084c91ed833d8fe6790bfdad5fa0026d06f06
ASG Kali new <vivek.k@prarang.in>
2025-11-01 11:25:10 +0530

    NewData

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  i18n: {
+    locales: ['hi'],
+    defaultLocale: 'hi',
+  },
+  async redirects() {
+    return [
+      {
+        source: '/%E0%A4%B2%E0%A5%87%E0%A4%96/:id/:slug*',
+        destination: '/लेख/:id/:slug*',
+        permanent: true,
+      },
+
+    ];
+  },
+  reactStrictMode: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
